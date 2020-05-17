4cce7e43d29937cc3bfbbffce39cf8dc8bb8e967
Amomomtszs <65496602+Amomomtszs@users.noreply.github.com>
2020-05-18 00:24:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='')
+bot = commands.Bot(command_prefix='*')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -14,8 +14,58 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def 草(ctx):
-    await ctx.send('pong')
+async def Ar(ctx):
+    await ctx.send('読んだか~い?')
 
+@bot.command()
+async def こんにちは(ctx):
+    await ctx.send('こん～')
+
+@bot.command()
+async def よろしくお願いします!(ctx):
+    await ctx.send('よろしくうううううう!!')
+
+@bot.command()
+async def よろしくお願いします(ctx):
+    await ctx.send('よろしくううううううううぇええええええい!!!')
+
+@bot.command()
+async def 死ね(ctx):
+    await ctx.send('は？そういうこといったらだめですよぉ!')
+
+@bot.command()
+async def こんばんは(ctx):
+    await ctx.send('こんばんわんこ!!')
+@bot.command()
+async def おやすみ(ctx):
+    await ctx.send('おやすみなさいませ')
+
+@bot.command()
+async def ちんちん(ctx):
+    await ctx.send('ちんちん大好き!!')
+
+@bot.command()
+async def 森(ctx):
+    await ctx.send('もりもり唐揚げ')
+
+@bot.command()
+async def メイ(ctx):
+    await ctx.send('メイ大好き!!')
+
+@bot.command()
+async def おはよう(ctx):
+    await ctx.send('おはよう!お腹すいた!金くれ!!')
+
+@bot.command()
+async def うんち(ctx):
+    await ctx.send('きたない')
+
+@bot.command()
+async def Discord(ctx):
+    await ctx.send('Discordは神!!!')
+
+@bot.command()
+async def うるさい(ctx):
+    await ctx.send('は？Ar様になんてことを!!!')
 
 bot.run(token)
