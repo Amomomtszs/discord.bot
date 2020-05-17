a7c98bd45dcf39c68d5a8f19525a310899f2f6b1
Amomomtszs <65496602+Amomomtszs@users.noreply.github.com>
2020-05-18 00:09:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,21 @@
-import discord
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+from discord.ext import commands
+import os
+import traceback
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+bot = commands.Bot(command_prefix='')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
 
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def 草(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
